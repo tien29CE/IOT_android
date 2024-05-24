928756ba7e8066c6b0a3d4251ee4c918aa5e089f
tien29CE <tien.le.290803@gmail.com>
2024-05-24 19:42:30 +0700

    xong bieu do nhung khong ket noi ada

--- app/src/main/java/bku/iot/asssigment_iot/MainActivity.java
+++ app/src/main/java/bku/iot/asssigment_iot/MainActivity.java
@@ -1,7 +1,10 @@
 package bku.iot.asssigment_iot;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,11 +36,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
+import android.widget.Toast;
+import android.content.Context;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity {
     MQTTHelper mqttHelper;
-    List<DataEntry> seriesData = new ArrayList<>();
-    List<DataEntry> seriesData1 = new ArrayList<>();
+    Button button;
+    int i = 0;
+    private static final Random RANDOM = new Random();
+    private LineGraphSeries<DataPoint> series, series1;
+    private int lastX = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,97 +65,50 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        GraphView graph = findViewById(R.id.temperature_linear_chart);
+        // data
+        series = new LineGraphSeries<DataPoint>();
+        series.setTitle("Temperature");
+        series.setColor(Color.RED);
+        series.setDrawDataPoints(true);
+        series.setDrawBackground(true);
+        graph.addSeries(series);
+        // customize a little bit viewport
+        Viewport viewport = graph.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxY(10);
+        viewport.setScrollable(true);
+        viewport.setBorderColor(100);
+        viewport.setBackgroundColor(Color.argb(40, 255,64, 25));
 
-        ///////////
-        AnyChartView tempChartView = findViewById(R.id.temperature_linear_chart);
-        APIlib.getInstance().setActiveAnyChartView(tempChartView);
+        GraphView graph1 = findViewById(R.id.humidity_linear_chart);
+        // data
+        series1 = new LineGraphSeries<DataPoint>();
+        series1.setTitle("Humidity");
+        series1.setColor(Color.rgb(0,191,255));
+        series1.setDrawDataPoints(true);
+        series1.setDrawBackground(true);
+        graph1.addSeries(series1);
+        // customize a little bit viewport
+        Viewport viewport1 = graph1.getViewport();
+        viewport1.setYAxisBoundsManual(true);
+        viewport1.setMinY(0);
+        viewport1.setMaxY(10);
+        viewport1.setScrollable(true);
+        viewport1.setBorderColor(100);
+        viewport1.setBackgroundColor(Color.argb(40, 77,136,255));
 
-        Cartesian cartesian = AnyChart.line();
+        button = findViewById(R.id.button);
+        if(button != null){
+            button.setOnClickListener((View.OnClickListener)(new View.OnClickListener() {
+                public final void onClick(View it) {
 
-        cartesian.animation(true);
-
-        cartesian.padding(10d, 20d, 5d, 20d);
-
-        cartesian.crosshair().enabled(true);
-        cartesian.crosshair()
-                .yLabel(true)
-                // TODO ystroke
-                .yStroke((Stroke) null, null, null, (String) null, (String) null);
-
-        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
-
-        cartesian.title("Temperature");
-
-        cartesian.yAxis(0).title("°C");
-        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
-
-        Set set = Set.instantiate();
-        set.data(seriesData);
-        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
-
-        Line series1 = cartesian.line(series1Mapping);
-        series1.color("#dc1e1e");
-        series1.hovered().markers().enabled(true);
-        series1.hovered().markers()
-                .type(MarkerType.CIRCLE)
-                .size(4d);
-        series1.tooltip()
-                .position("right")
-                .anchor(Anchor.LEFT_CENTER)
-                .offsetX(5d)
-                .offsetY(5d);
-
-        cartesian.legend().enabled(true);
-        cartesian.legend().fontSize(13d);
-        cartesian.legend().padding(0d, 0d, 10d, 0d);
-
-        tempChartView.setChart(cartesian);
-        //=====================================//
-        AnyChartView humiChartView = findViewById(R.id.humidity_linear_chart);
-        APIlib.getInstance().setActiveAnyChartView(humiChartView);
-
-        Cartesian cartesian1 = AnyChart.line();
-
-        cartesian1.animation(true);
-
-        cartesian1.padding(10d, 20d, 5d, 20d);
-
-        cartesian1.crosshair().enabled(true);
-        cartesian1.crosshair()
-                .yLabel(true)
-                // TODO ystroke
-                .yStroke((Stroke) null, null, null, (String) null, (String) null);
-
-        cartesian1.tooltip().positionMode(TooltipPositionMode.POINT);
-
-        cartesian1.title("Humidity");
-
-        cartesian1.yAxis(0).title("%");
-        cartesian1.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
-
-        Set set1 = Set.instantiate();
-        set1.data(seriesData1);
-        Mapping series2Mapping = set1.mapAs("{ x: 'x', value: 'value' }");
-
-        Line series2 = cartesian1.line(series2Mapping);
-//        series1.name("Brandy");
-        series2.color("#0a31cf");
-        series2.hovered().markers().enabled(true);
-        series2.hovered().markers()
-                .type(MarkerType.CIRCLE)
-                .size(4d);
-        series2.tooltip()
-                .position("right")
-                .anchor(Anchor.LEFT_CENTER)
-                .offsetX(5d)
-                .offsetY(5d);
-
-        cartesian1.legend().enabled(true);
-        cartesian1.legend().fontSize(13d);
-        cartesian1.legend().padding(0d, 0d, 10d, 0d);
-
-        humiChartView.setChart(cartesian1);
-        startMQTT();
+                    // displaying a toast message
+                    addEntry();
+                }
+            }));
+        }
     }
 
     public void sendDataMQTT(String topic, String value){
@@ -175,10 +145,10 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("TEST", topic + " " + message.toString());
                 if(topic.contains("cambien1")){
 //                    txtTemp.setText(message.toString() + "°C");
-                    seriesData.add(new ValueDataEntry(date.toString(), Integer.parseInt(message.toString())));
+                    series.appendData(new DataPoint(Integer.parseInt(date.toString()), Integer.parseInt(message.toString())), true, 30);
                 }else if(topic.contains("cambien2")) {
 //                    txtHumi.setText(message.toString() + "%");
-                    seriesData1.add(new ValueDataEntry(date.toString(), Integer.parseInt(message.toString())));
+                    series1.appendData(new DataPoint(Integer.parseInt(date.toString()), Integer.parseInt(message.toString())), true, 30);
                 }else if(topic.contains("cambien3")){
 //                    txtLux.setText(message.toString() + "lux");
                 }else if(topic.contains("nutnhan1")){
@@ -201,5 +171,38 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // we add 100 new entries
+                for (int i = 0; i < 100; i++) {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+//                            addEntry();
+                        }
+                    });
+
+                    // sleep to slow down the add of entries
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // manage error ...
+                    }
+                }
+            }
+        }).start();
+    }
+    private void addEntry() {
+        // here, we choose to display max 10 points on the viewport and we scroll to end
+        series.appendData(new DataPoint(lastX++, RANDOM.nextDouble() * 10d), true, 30);
+        series1.appendData(new DataPoint(lastX++, RANDOM.nextDouble() * 10d), true, 30);
     }
 }
